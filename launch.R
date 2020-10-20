74ce5dff1b2bca0593dca7b4a0609cad29905b3a
James Austin Wehrwein <jawehrwein@gmail.com>
2020-10-20 07:22:12 -0500

    big ol' updates

--- launch.R
+++ launch.R
@@ -2,3 +2,9 @@ library(blogdown)
 serve_site()
 stop_server()
 
+#old soccer button
+                        <li>
+                            <a data-hint="Soccer" title="EPL" href="{{ .Site.BaseURL }}EPL/">
+                                <i class="fa fa-futbol-o fa-lg"></i>
+                            </a>
+                        </li>
